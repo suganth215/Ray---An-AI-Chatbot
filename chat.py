a97ad40fbb9fc9d10c4d75e9a6abab60c9f53fa2
suganth215 <59024917+suganth215@users.noreply.github.com>
2021-03-23 16:46:29 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,52 @@
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+    
+FILE = "data.pth"
+data = torch.load(FILE)
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Ray"
+endbye = ["Hope I have cleared your doubts, Thanks and bye!","It was my pleasure helping you, hope you enjoy our cafe","Thank you for reaching out, bye!"]
+print("Type your messages, Type 'quit' to exit")
+while True:
+    sentence = input('You: ')
+    if sentence == "quit":
+        print(f"{bot_name}: {random.choice(endbye)}")
+        break
+    
+    sentence = tokenize(sentence)
+    x = bag_of_words(sentence, all_words)
+    x = x.reshape(1, x.shape[0])
+    x = torch.from_numpy(x)
+    
+    output = model(x)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+    
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    nope = ["Oops! I did not understand what you are talking about","Sorry, I did not get it, Can you explain please", "I didn't quite get that.."]
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: {random.choice(nope)}")
+    
